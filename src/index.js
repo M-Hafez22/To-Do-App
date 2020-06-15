717849c3a0dbcc3066e4abe127fee004faddf770
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-06-15 04:42:10 +0200

    Create the store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+import todos from './reducers/todos';
+import { Provider } from 'react-redux';
+
+const store = createStore(todos, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
