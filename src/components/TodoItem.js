641fb29e62cdcab5822820cefd859ff8d2b01511
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-06-15 08:30:26 +0200

    Add visible button for toggling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function TodoItem({text, toggleItem, deleteItem }){
   return(
     <div className='todoItem'>
+
       <li onClick={toggleItem}> {text} </li>
-      <button onClick={deleteItem}>Delete </button>
+      <div className='buttons'>
+        <button className='item-button' onClick={toggleItem}>Done </button>
+        <button className='item-button' onClick={deleteItem}>Delete </button>
+      </div>
     </div>
   )
 }
