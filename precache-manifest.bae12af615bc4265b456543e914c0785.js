d32bbdf1135bf7cd8aa4806dc77ef1db5a544443
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-07-13 22:39:27 +0200

    Updates

---
+++ precache-manifest.bae12af615bc4265b456543e914c0785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c63c88c356ebbfc1d847b9ae83d77b",
+    "url": "/to_do/index.html"
+  },
+  {
+    "revision": "d79b2aa22eae6c956e8e",
+    "url": "/to_do/static/css/main.718d5657.chunk.css"
+  },
+  {
+    "revision": "391e5dc73523c86d13dd",
+    "url": "/to_do/static/js/2.6e355c6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to_do/static/js/2.6e355c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79b2aa22eae6c956e8e",
+    "url": "/to_do/static/js/main.47fea292.chunk.js"
+  },
+  {
+    "revision": "784e23e9866ecbaa698f",
+    "url": "/to_do/static/js/runtime-main.e15a336f.js"
+  }
+]);
