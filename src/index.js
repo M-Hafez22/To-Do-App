7dc39ae458654c0000c3ab13fff5a5f6df2b4c56
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-07-26 10:13:44 +0200

    :package: upgrade to React 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,13 +10,12 @@ import { Provider } from 'react-redux';
 
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store} >
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
