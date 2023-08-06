7981fe488744ba1a3581c1ee9ea690efab18a681
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-08-06 17:44:34 +0300

    ♻️ Reduce JS control over colors

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { activeColor } from '../helper/colors';
 
 export default function FilterButton({type, onClick, current}){
 
-  const currentFilterStyle = {
-    color : activeColor,
-    borderColor : activeColor
-  }
-
   return(
     <button
-      style={ current ?  currentFilterStyle : null}
+    className={`${current && "currentFilter"}`}
       onClick={onClick}>{type}
     </button>
   )
